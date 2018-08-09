857badb4eaa7c8ee96fe5389525d76f26ad67838
Ghabriel Calsa Nunes <gcnunes@SEF.SC.GOV.BR>
2018-08-09 12:45:11 -0300

    Improved the text positioning and added a box around it

--- src/render-textboxes.cpp
+++ src/render-textboxes.cpp
@@ -12,13 +12,30 @@ void renderTextBoxes(
     engine::resourcesystem::ResourceStorage& storage
 ) {
     using engine::entitysystem::Entity;
+    constexpr int textBoxMargin = 5;
+    constexpr int textMargin = 5;
+    Camera& camera = storage.get<Camera>("camera");
+    const int textBoxWidth = camera.width - 2 * textBoxMargin;
+    const int textBoxHeight = camera.height / 5;
+    const int textBoxX = textBoxMargin;
+    const int textBoxY = camera.height - textBoxHeight - textBoxMargin;
+    const int textX = textBoxX + textMargin;
+    const int textY = textBoxY + textMargin;
 
     manager.forEachEntity<TextBox>(
         [&](
             Entity entity,
             TextBox& textBox
         ) {
+
+            sf::RectangleShape rect(sf::Vector2f(textBoxWidth, textBoxHeight));
+            rect.setPosition(textBoxX, textBoxY);
+            rect.setFillColor(sf::Color::White);
+            window.draw(rect);
+
             sf::Text text(textBox.content, storage.get<sf::Font>("font-arial"));
+            text.setPosition(textX, textY);
+            text.setFillColor(sf::Color::Black);
             text.setCharacterSize(30);
             window.draw(text);
         }
