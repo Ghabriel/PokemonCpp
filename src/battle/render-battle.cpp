6b7a9bb9b418900868a0c7d55d030f400e2879f5
Ghabriel <ghabriel.nunes@gmail.com>
2018-08-26 16:30:11 -0300

    Complete battle 'info card'

--- src/battle/render-battle.cpp
+++ src/battle/render-battle.cpp
@@ -38,6 +38,18 @@ void renderFoePokemon(
     window.draw(sprite);
 }
 
+sf::Color getHealthBarColor(float percentage) {
+    if (percentage > 0.5) {
+        return sf::Color::Green;
+    }
+
+    if (percentage > 0.25) {
+        return sf::Color::Yellow;
+    }
+
+    return sf::Color::Red;
+}
+
 void renderInfoCard(
     sf::RenderWindow& window,
     ResourceStorage& storage,
@@ -46,10 +58,74 @@ void renderInfoCard(
     float baseY,
     bool isAlly
 ) {
-    std::string content = std::to_string(pokemon.currentHP) + "/" + std::to_string(pokemon.stats[0]);
-    sf::Text text(content, storage.get<sf::Font>("font-arial"));
-    text.setPosition(baseX, baseY);
-    window.draw(text);
+    constexpr size_t hpContainerWidth = 200;
+    constexpr size_t hpContainerHeight = 18;
+    constexpr size_t hpContainerYOffset = 25;
+    constexpr size_t hpMargin = 2;
+
+    sf::Font& font = storage.get<sf::Font>("font-arial");
+    sf::Text name(pokemon.displayName, font);
+    name.setCharacterSize(20);
+    name.setFillColor(sf::Color::White);
+    name.setOutlineColor(sf::Color::Black);
+    name.setOutlineThickness(1);
+    name.setPosition(baseX, baseY);
+    window.draw(name);
+
+    sf::Text level("Lv." + std::to_string(pokemon.level), font);
+    level.setCharacterSize(20);
+    level.setFillColor(sf::Color::White);
+    level.setOutlineColor(sf::Color::Black);
+    level.setOutlineThickness(1);
+    level.setPosition(baseX + hpContainerWidth, baseY);
+    window.draw(level);
+
+    sf::RectangleShape hpContainer({hpContainerWidth, hpContainerHeight});
+    hpContainer.setPosition(baseX, baseY + hpContainerYOffset);
+    hpContainer.setFillColor(sf::Color::Black);
+    window.draw(hpContainer);
+
+    float hpPercentage = static_cast<float>(pokemon.currentHP) / pokemon.stats[0];
+    sf::RectangleShape hpBar({
+        (hpContainerWidth - 2 * hpMargin) * hpPercentage,
+        hpContainerHeight - 2 * hpMargin
+    });
+    hpBar.setPosition(baseX + hpMargin, baseY + hpContainerYOffset + hpMargin);
+    hpBar.setFillColor(getHealthBarColor(hpPercentage));
+    window.draw(hpBar);
+
+    if (isAlly) {
+        size_t hpTextY = baseY + hpContainerYOffset + hpContainerHeight;
+        constexpr size_t expContainerWidth = 200;
+        constexpr size_t expContainerHeight = 5;
+        constexpr size_t expContainerYOffset = 30;
+        constexpr size_t expMargin = 1;
+
+        sf::Text hpText(
+            std::to_string(pokemon.currentHP) + "/" + std::to_string(pokemon.stats[0]),
+            font
+        );
+        hpText.setCharacterSize(25);
+        hpText.setFillColor(sf::Color::White);
+        hpText.setOutlineColor(sf::Color::Black);
+        hpText.setOutlineThickness(1);
+        hpText.setPosition(baseX, hpTextY);
+        window.draw(hpText);
+
+        sf::RectangleShape expContainer({expContainerWidth, expContainerHeight});
+        expContainer.setPosition(baseX, hpTextY + expContainerYOffset);
+        expContainer.setFillColor(sf::Color::Black);
+        window.draw(expContainer);
+
+        float expPercentage = 0.25; // TODO
+        sf::RectangleShape expBar({
+            (expContainerWidth - 2 * expMargin) * expPercentage,
+            expContainerHeight - 2 * expMargin
+        });
+        expBar.setPosition(baseX + expMargin, hpTextY + expContainerYOffset + expMargin);
+        expBar.setFillColor(sf::Color::Cyan);
+        window.draw(expBar);
+    }
 }
 
 void renderBattle(
@@ -65,6 +141,11 @@ void renderBattle(
             Battle& battle
         ) {
             Camera& camera = storage.get<Camera>("camera");
+            sf::Sprite background(storage.get<sf::Texture>("battle-bg-1"));
+            background.setScale(camera.width / 512.0, camera.height / 288.0);
+            background.setPosition(0, 0);
+            window.draw(background);
+
             if (!manager.hasComponent<Fainted>(battle.playerPokemon)) {
                 Pokemon& playerPokemon = manager.getData<Pokemon>(battle.playerPokemon);
                 renderAllyPokemon(window, camera, storage, playerPokemon);
@@ -72,7 +153,7 @@ void renderBattle(
                     window,
                     storage,
                     playerPokemon,
-                    7 * camera.width / 10,
+                    6 * camera.width / 10,
                     3 * camera.height / 5,
                     true
                 );
